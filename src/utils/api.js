e483e1510f2597bab21e85fc25a996ff5d06c8f6
elultra <zhuceyong666@outlook.com>
2023-03-06 00:47:30 +1000

    feat: change axios url

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
 export const getWeathers = () => {
-      return axios.get('http://localhost:8080/api/v1/weathers');
+      return axios.get('https://ericweatherapp.herokuapp.com/api/v1/weathers');
 }
