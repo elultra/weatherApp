86dd4bd8a50d0c3cd60cbf0f0443e85b26ffab35
elultra <102572669+elultra@users.noreply.github.com>
2023-03-06 23:56:06 +1000

    Dev geo (#2)
    * feat: add geo to search weather function
    
    * fix: remove console

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
+import { useState } from 'react';
 import './App.css';
 import Card from './components/Card/Card';
 import Header from './components/Header';
 import Wave from './components/Wave/Wave';
 
 function App() {
+      const [country, setCountry] = useState("");
+      const handleChange = (val) =>{
+            setCountry(val)
+      }
   return (
       <div className='App'>
-            <Header />
-            <Card />
+            <Header onChange={handleChange}/>
+            <Card country={country}/>
             <Wave />
       </div>
   );
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import { BiMenu } from "react-icons/bi";
 import Clock from "./Clock/Clock";
 import weaths from "../images/weaths.png";
 
-const Header = () => {
+const Header = (props) => {
+      const [inputValue, setInputValue] = useState("");
+      const handleChange = (e) =>{
+            setInputValue(e.target.value)
+            props.onChange(e.target.value)
+      }
 	return (
 		<>
 			<div className="bg-[#003366] fixed w-full top-0 left-0 flex justify-between items-center px-[15px]">
@@ -18,6 +23,8 @@ const Header = () => {
 					<input
 						type="text"
 						className="text-lg mr-10 text-white bg-transparent w-2 h-3 p-3 border-2 border-white rounded-full relative focus:w-1/4"
+                                    value={inputValue}
+                                    onChange={handleChange}
 					></input>
                               <div className="w-1 h-3 absolute bg-white top-[40px] right-[52px] rotate-[135deg]"></div>
 				</div>
