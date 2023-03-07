40ab3302b1fe4e97c1719cc474ec27b56510baa6
elultra <102572669+elultra@users.noreply.github.com>
2023-03-07 20:46:35 +1000

    Weat 3 card header data (#3)
    * feat: fetching data to card header
    
    * feat: fetch country and render

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,41 +1,44 @@
-import React from 'react';
-import CardHeader from './CardHeader';
-import './loadingAnimation.css';
-import Forecast from '../Forecast/Forecast';
-import SocialMedia from '../SocialMedia/SocialMedia';
-import { useState, useEffect } from 'react';
-import { getWeathers } from '../../utils/api';
+import React from "react";
+import CardHeader from "./CardHeader";
+import "./loadingAnimation.css";
+import Forecast from "../Forecast/Forecast";
+import SocialMedia from "../SocialMedia/SocialMedia";
+import { useState, useEffect } from "react";
+import { getWeathers } from "../../utils/api";
 
 const Card = (props) => {
-      const [loading, isLoading] = useState(false);
-      const [data, setData] = useState([]);
-      useEffect(() => {
-            const getData = async () => {
-                        isLoading(true);
-                        const weatherData = await getWeathers(props.country);
-                        setData(weatherData.data);
-                        isLoading(false)
-                  }
-            getData();
-      }, [props.country])
-
-      return (
-            <div className='w-8/12 m-auto z-99 bg-white/10 rounded-lg shadow-lg'>
-                  <CardHeader />
-                  {loading?
-                        (
-                              <div className='flex justify-center items-center pt-20'>
-                                    <div className="loader ease-linear rounded-full border-4 border-t-4 border-gray-200 h-12 w-12 mb-4"></div>
-                              </div>
-                        ) : (
-                              <div className='flex justify-between rounded-b-xl z-5 shadow-lg'>
-                                    <SocialMedia />
-                                    <Forecast data={data} />
-                              </div>
-                        )
-                  }
-            </div>
-      );
-}
+	const [loading, isLoading] = useState(false);
+      const [country, setCountry] = useState({});
+	const [data, setData] = useState([]);
+	useEffect(() => {
+		const getData = async () => {
+			isLoading(true);
+			const {data} = await getWeathers(props.country);
+                  const country = data.slice(-1)[0];
+                  data.pop();
+                  setCountry(country)
+			setData(data);
+			isLoading(false);
+		};
+		getData();
+	}, [props.country]);
+	return (
+		<div className="w-8/12 m-auto z-99 bg-white/10 rounded-lg shadow-lg">
+			{data.length > 0 ? (
+				<>
+					<CardHeader data={data} country={country}/>
+					<div className="flex justify-between rounded-b-xl z-5 shadow-lg">
+						<SocialMedia />
+						<Forecast data={data} />
+					</div>
+				</>
+			) : (
+				<div className="flex justify-center items-center pt-20">
+					<div className="loader ease-linear rounded-full border-4 border-t-4 border-gray-200 h-12 w-12 mb-4"></div>
+				</div>
+			)}
+		</div>
+	);
+};
 
 export default Card;
--- src/components/Card/CardHeader.jsx
+++ src/components/Card/CardHeader.jsx
@@ -1,26 +1,37 @@
-function CardHeader() {
-      return (
-            <div className='pt-20 pb-10 px-16 flex justify-between bg-[#003366] rounded-t-lg'>
-                  <div>
-                        <p className='text-lg text-white capitalize font-semibold'><span className='text-[80px] text-white'>19°</span>possible light rain</p>
-                        <div className='gap-6 w-full mx-auto flex jusity-center items-center text-white pt-5'>
-                              <div>
-                                    <p className='uppercase font-semibold'>humidity</p>
-                                    <p>47%</p>
-                              </div>
-                              <hr className='h-[40px] bg-white w-[1px]' />
-                              <div>
-                                    <p className='uppercase font-semibold'>precipitation</p>
-                                    <p>5mm</p>
-                              </div>
-                        </div>
-                  </div>
-                  <div className="flex flex-col justify-center self-start">
-                        <p className='text-white text-3xl font-bold tracking-wider'>Sydney</p>
-                        <hr className="w-full h-[2px] bg-white mt-0.5"/>
-                  </div>
-            </div>
-      )
+function CardHeader(props) {
+	const { data, country } = props;
+      console.log(country)
+	return (
+		<div className="pt-20 pb-10 px-16 flex justify-between bg-[#003366] rounded-t-lg">
+			<div>
+				<div className="flex items-end pb-4">
+					<p className="text-6xl text-white font-semibold pr-6">
+							{Math.round(data[0].main.temp)}°C
+					</p>
+					<p className="text-lg text-white capitalize">
+							{data[0].weather[0].description}
+					</p>
+				</div>
+				<div className="gap-6 w-full mx-auto flex jusity-center items-center text-white pt-5">
+					<div>
+						<p className="uppercase font-semibold">humidity</p>
+						<p>{data[0].main.humidity}%</p>
+					</div>
+					<hr className="h-[40px] bg-white w-[1px]" />
+					<div>
+						<p className="uppercase font-semibold">wind speed</p>
+						<p>{data[0].wind.speed}m/s</p>
+					</div>
+				</div>
+			</div>
+			<div className="flex flex-col justify-center self-start">
+				<p className="text-white text-3xl font-bold tracking-wider">
+                              {props.country.country}
+				</p>
+				<hr className="w-full h-[2px] bg-white mt-0.5" />
+			</div>
+		</div>
+	);
 }
 
 export default CardHeader;
