dd8da92249caf9b34174a92af61a3a014dfc9479
elultra <zhuceyong666@outlook.com>
2023-03-12 21:49:27 +1000

    fix: disable wave component

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { useState } from 'react';
 import './App.css';
 import Card from './components/Card/Card';
 import Header from './components/Header';
-import Wave from './components/Wave/Wave';
+// import Wave from './components/Wave/Wave';
 
 function App() {
       const [country, setCountry] = useState("");
@@ -10,14 +10,14 @@ function App() {
             setCountry(val)
       }
       return (
-            <div className='App flex flex-col overflow-auto relative w-ful h-[100vh]'>
+            <div className='App flex flex-col overflow-auto relative w-full h-[100vh]'>
                   <Header onChange={handleChange} />
                   <div className="mt-16 md:min-w-7/12 md:w-8/12 md:max-w-9/12 m-auto bg-white/10 rounded-lg shadow-lg overflow-x-auto z-10 xs:w-full">
                         <Card country={country} />
                   </div>
-                  <div className='bottom-0 hidden md:block'> 
+                  {/* <div className='bottom-0 hidden md:inline-block max-w-[1536px] overflow-invisible'> 
                         <Wave />
-                  </div>
+                  </div> */}
             </div>
       );
 }
