67217a551a4b1d5863e572eb957de5538fccd773
elultra <zhuceyong666@outlook.com>
2023-06-11 00:35:49 +1000

    chore: polish design, remove clock

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,29 @@
-import { useState } from 'react';
-import './App.css';
-import Card from './components/Card/Card';
-import Header from './components/Header';
+import { useState } from "react";
+import "./App.css";
+import Card from "./components/Card/Card";
+import Header from "./components/Header";
 // import Wave from './components/Wave/Wave';
 // interface AppProps{
 //       onChange: (val: string) =>void
 // }
 function App() {
-      const [country, setCountry] = useState<any>("");
-      const handleChange = (val:string) => {
-            setCountry(val)
-      }
-      return (
-            <div className='App flex flex-col overflow-auto relative w-full h-[100vh]'>
-                  <Header onChange={handleChange} />
-                  <div className="mt-16 md:min-w-7/12 md:w-8/12 md:max-w-9/12 m-auto bg-white/10 rounded-lg shadow-lg overflow-x-auto z-10 xs:w-full">
-                        <Card country={country} />
-                  </div>
-                  {/* <div className='bottom-0 hidden md:inline-block max-w-[1536px] overflow-invisible'> 
+    const [country, setCountry] = useState<any>("");
+    const handleChange = (val: string) => {
+        setCountry(val);
+    };
+    return (
+        <div className="App flex flex-col overflow-auto relative w-full h-[100vh]">
+            <Header onChange={handleChange} />
+            <div className="flex justify-center items-center">
+                <div className="mt-16 md:min-w-7/12 md:w-8/12 md:max-w-9/12 m-auto bg-white/10 rounded-lg shadow-lg overflow-x-auto z-10 xs:w-full">
+                    <Card country={country} />
+                </div>
+            </div>
+            {/* <div className='bottom-0 hidden md:inline-block max-w-[1536px] overflow-invisible'> 
                         <Wave />
                   </div> */}
-            </div>
-      );
+        </div>
+    );
 }
 
 export default App;
